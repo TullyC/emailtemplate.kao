2dc8bc41c9a9becacc08b71715f7ce443ca4d7c8
Chris Tully <chris.tully@profitdriverpro.com>
2018-04-03 17:22:42 -0400

    Purchase Summary Header Styling
    Initial styles and minor code structure changes.

--- resources/views/emailTemplates/summaryHeader.blade.php
+++ resources/views/emailTemplates/summaryHeader.blade.php
@@ -1,5 +1,4 @@
-<table>
-	<table>
+<table class="header_add">
 	<tr>
 		<td>
 			<p><b>Addressed For:</b></p>
@@ -17,32 +16,46 @@
 	</tr>
 	<tr>
 		<td>
-			<p>{ City }  { State } { Zip Code }</p>
+			<p>{ City },  { State } { Zip Code }</p>
 		</td>
 	</tr>
 </table>
 
-<table border="2 px solid black;">
-	<tr>
-		<td>
-			<h1>Vehicle Contract Summary</h1>
+<table class="header_contract" style="width:550px; height:150px;">
+	<tr style="height:150px;" >
+		<td style="width: 210px; height: 150px; padding:0;">
+			<table style="height: 150px; display: inline-block;">
+				<tr style="height: 100%; display: block;">
+					<td style="padding-top:35px;">
+						<h1>Vehicle Contract Summary</h1>
+					</td>
+				</tr>
+			</table>
 		</td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/default.jpg" alt="Dealer Logo">
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<p>{ dealership } { Logo }</p>
+		<td style="padding:0;">
+			<table class="dealer_info_wrap" style="padding:0;height:150px;" >
+
+				<tr style="width:300px; display:block; padding:0; height:100px;">
+					<td style="display:inline-block; padding:0;">
+						<img class="big_dealer_logo" src="/default.jpg" alt="Dealer Logo">
+					</td>					
+				</tr>	
+				<tr  style="display:block; height:50px; padding:0;" >
+					<td style="display:block; height:50px; padding:0;">
+						<p style="width:50%;padding:0; padding-left:10px;display: inline-block; ">{ dealership }</p>
+						<img  class="little_dealer_logo" src="/default.jpg" alt="Dealer Logo"/>
+
+					</td>
+					
+				</tr>
+			</table>
 		</td>
 	</tr>
 </table>
 
-<table>
+<table class="vehicle_info" style="width:100%;">
 	<tr>
-		<td>
+		<td style="padding:0;">
 			<h2>Vehicle:</h2>
 		</td>
 		<td>
@@ -59,8 +72,12 @@
 		</td>
 	</tr>
 </table>
+	
+</table>
 
-<br />
+
+
+<hr><!-- Drop shadows are not supported in older browsers -->
 
 <table>
 	<tr>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,139 @@
 @include('layouts.header')
+
+
+<style>
+	body, #bodyTable { 
+		margin:0; 
+		padding:0;
+		width:600px;
+		border:1px solid red;
+		margin:0 auto;
+	}
+	 body{
+	 	padding:75px 150px 0px 75px;
+	 }
+ 	img, a img {
+ 		border:0; 
+ 		outline:none; 
+ 		text-decoration:none;
+ 	 }
+ 	.imageFix { 
+ 		display:block; 
+ 	}
+ 	table, td {
+ 		border-collapse:collapse;
+ 	}
+	*{
+		font-family:‘Times New Roman’, Times, serif;
+		font-size:1em;
+	}
+	h1{
+		font-size:1.5em;
+	}
+	h2{
+		font-size:1.4em;
+	}
+	h3{
+		font-size:1.3em;
+	}
+	hr{
+		width:95%;
+		display:block;
+		color:#000000;
+		background-color:;
+		 background-color: #000000; height: 3px; border: 0;
+
+	}
+	table{
+		/*border: 1px solid black;
+		border-spacing: 15px;*/
+	}
+	table td{
+		padding:5px;
+	}
+
+	
+
+	 tr{
+	 	padding:15px;
+	 }
+
+	 /*utility Class remove from production*/
+	 table tr{
+	 	border:1px dotted black;
+	 }
+ /**
+  *  Specific for summaryHeader.blade
+  */
+ .header_add{
+ 	border:1px solid black;
+ 	margin-bottom:15px;
+ }
+ .header_contract{
+ 	width:550px !important;
+ 	height:150px !important;
+ 	border:5px solid black;
+ }
+ .header_contract tr{
+ 	margin: 0 auto;
+ }
+ .header_contract h1{
+ 	width: 200px;
+    padding: 0;
+    margin: 0;
+    text-align:center;
+ }
+
+
+ .dealer_info_wrap{
+ 	padding: 0;
+    height: 150px;
+    display: block;
+ }
+ .dealer_info_wrap tr td{
+ 	padding:0;
+ }
+
+ .big_dealer_logo{
+ /*	width:150px;
+ 	height:150px;*/
+ 	margin: 0 auto;
+    display: block;
+ }
+
+ .little_dealer_logo{
+ 	display:inline-block;
+ 	
+ 	margin-top:10px;
+ 	float:right
+ 	padding-right:10px;
+ }
+ 
+ .vehicle_info td{
+ 	padding:0;
+ 	display:inline-block;
+
+
+ }
+  .vehicle_info tr{
+ 	padding:0;
+ 	display:inline-block;
+ 	height:50px;
+ 	width:100%;
+
+
+ }
+
+
+</style>
+
 <body>
 	<table>
-		@yield('content')
+		<tr>
+			<td>
+				@yield('content')
+			</td>
+		</tr>	
 	</table>
 </body>
 @include('layouts.footer')
